e2ad3489920624d34419b5c6e30734bb86fc8626
Войченко Виталий <mrabbat69@gmail.com>
2021-07-23 02:32:42 +0700

    Update webScraping.js
    добавлена возможность сохранять спаршенные данные с сайта в json файл (над представлением еще надо поработать)

--- webScraping/webScraping.js
+++ webScraping/webScraping.js
@@ -1,35 +1,33 @@
 const cheerio = require("cheerio"); 
 const axios = require("axios");  
+const fs = require("fs")
 
+
+ if (fs.existsSync('restaurant_images.json') && (fs.existsSync('restaurant_names.json'))) {
+  fs.unlinkSync('restaurant_images.json');
+  fs.unlinkSync('restaurant_names.json');
+ }
 
 axios.get('https://www.delivery-club.ru/krasnoyarsk').then(urlResponse => {
 const $ = cheerio.load (urlResponse.data);
 
- $('a.vendor-item__link').each((i, element) => {
-   
-     const restaurantLinkBuffer = $(element)
-      .attr ('href');
+        $('a.vendor-item__link').each((i, element) => {
+       
+            const restaurantImageBuffer = $(element)
+           .find('span.vendor-item__cover')
+           .attr ('data-src');
 
-      restaurantLink = 'https://www.delivery-club.ru' + restaurantLinkBuffer;
-    const restaurantImageBuffer = $(element)
-      .find('span.vendor-item__cover')
-      .attr ('data-src');
+          restaurantImage = restaurantImageBuffer.slice (0, restaurantImageBuffer.indexOf ('?'));
+         
+      fs.appendFileSync('restaurant_images.json', `\n${restaurantImage}`); //записываем ссылки на фото ресторанов в файл
+      });
 
-      restaurantImage = restaurantImageBuffer.slice (
-        0, restaurantImageBuffer.indexOf ('?')
-        );
+      $('span.vendor-item__wrap').each((i, element) => {
 
-    console.log (restaurantImage + '\n' + restaurantLink);
-    console.log ('-------------------\n');
-  });
+          const restaurantName = $(element)
+          .find ('h3.vendor-item__title-text')
+          .text(); 
 
-  $('span.vendor-item__wrap').each((i, element) => {
-
-    const restaurantName = $(element)
-      .find ('h3.vendor-item__title-text')
-      .text(); 
-
-    console.log (restaurantName);
-    console.log ('-------------------\n');
-  });
+        fs.appendFileSync('restaurant_names.json', `\n${restaurantName}`); //записываем названия ресторанов в файл
+        });
 });
