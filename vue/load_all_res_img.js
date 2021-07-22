7da259fc4f0ee45bdeb6dd908c5d70444212954e
Войченко Виталий <mrabbat69@gmail.com>
2021-07-23 02:35:17 +0700

    Create load_all_res_img.js
    неавтоматическая загрузка изображений на сайт через vue

---
+++ vue/load_all_res_img.js
@@ -0,0 +1,15 @@
+new Vue ({
+	el: '.restaurant-list',
+	data: {
+		imgpath1: 'https://www.delivery-club.ru/naturmort/608c8b95484ed_480x300.jpg',
+		imgpath2: 'https://www.delivery-club.ru/naturmort/602f8ba2a69e0_480x300.jpg',
+		imgpath3: 'https://www.delivery-club.ru/naturmort/5f9c44d402ddc_480x300.jpg',
+		imgpath4: 'https://www.delivery-club.ru/naturmort/602ccf5221e3a_480x300.jpg',
+		imgpath5: 'https://www.delivery-club.ru/naturmort/6000040_480x300.jpg',
+		imgpath6: 'https://www.delivery-club.ru/naturmort/2000039_480x300.jpg',
+		imgpath7: 'https://www.delivery-club.ru/naturmort/5dd79f4d62d4f_480x300.jpg',
+		imgpath8: 'https://www.delivery-club.ru/naturmort/5f97db189595d_480x300.jpg',
+		imgpath9: 'https://www.delivery-club.ru/naturmort/18000032_480x300.jpg'
+	}
+});
+
