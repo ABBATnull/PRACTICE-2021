03079e473c5a0393990647fc8229f866b6799ae5
Войченко Виталий <mrabbat69@gmail.com>
2021-07-23 14:32:04 +0700

    Update and rename load_choosen_res_img.js to load_choosen_res_img_del_restaurant.js

---
+++ vue/load_choosen_res_img_del_restaurant.js
@@ -0,0 +1,53 @@
+new Vue ({
+	el: '.clearfix',
+	data: {
+		choosen_rest_img: ' ',
+		Restaurants: 'Restaurants'
+	},
+
+	methods: {
+		choice1 (){
+			this.choosen_rest_img = 'https://www.delivery-club.ru/naturmort/608c8b95484ed_480x300.jpg';
+			this.Restaurants = ' ';
+		},
+		choice2 (){
+			this.choosen_rest_img = 'https://www.delivery-club.ru/naturmort/602f8ba2a69e0_480x300.jpg';
+			this.Restaurants = ' ';
+		},
+
+		choice3 (){
+			this.choosen_rest_img = 'https://www.delivery-club.ru/naturmort/5f9c44d402ddc_480x300.jpg';
+			this.Restaurants = ' ';
+		},
+
+		choice4 (){
+			this.choosen_rest_img = 'https://www.delivery-club.ru/naturmort/602ccf5221e3a_480x300.jpg';
+			this.Restaurants = ' ';
+		},
+
+		choice5 (){
+			this.choosen_rest_img = 'https://www.delivery-club.ru/naturmort/6000040_480x300.jpg';
+			this.Restaurants = ' ';
+		},
+
+		choice6 (){
+			this.choosen_rest_img = 'https://www.delivery-club.ru/naturmort/2000039_480x300.jpg';
+			this.Restaurants = ' ';
+		},
+
+		choice7 (){
+			this.choosen_rest_img = 'https://www.delivery-club.ru/naturmort/5dd79f4d62d4f_480x300.jpg';
+			this.Restaurants = ' ';
+		},
+
+		choice8 (){
+			this.choosen_rest_img = 'https://www.delivery-club.ru/naturmort/5f97db189595d_480x300.jpg';
+			this.Restaurants = ' ';
+		},
+
+		choice9 (){
+			this.choosen_rest_img = 'https://www.delivery-club.ru/naturmort/18000032_480x300.jpg';
+			this.Restaurants = ' ';
+		}
+	}
+});
