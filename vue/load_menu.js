0bd35a157309318d38230ef13921c6171f668507
Войченко Виталий <mrabbat69@gmail.com>
2021-07-23 20:53:12 +0700

    Add files via upload

---
+++ vue/load_menu.js
@@ -0,0 +1,28 @@
+new Vue ({
+	el: '.menu-list',
+	data: {
+		imgpath1: 'https://www.delivery-club.ru/media/cms/relation_product/30692/310020415_m650.jpg',
+		imgpath2: 'https://www.delivery-club.ru/media/cms/relation_product/30692/311151365_m650.jpg',
+		imgpath3: 'https://www.delivery-club.ru/media/cms/relation_product/30692/311151365_m650.jpg',
+		imgpath4: 'https://www.delivery-club.ru/media/cms/relation_product/30692/310020373_m650.jpg',
+		imgpath5: 'https://www.delivery-club.ru/media/cms/relation_product/30692/310020371_m650.jpg',
+		imgpath6: 'https://www.delivery-club.ru/media/cms/relation_product/30692/310020372_m650.jpg',
+		imgpath7: 'https://www.delivery-club.ru/media/cms/relation_product/30692/310020374_m650.jpg',
+
+		name1: 'Шефбургер Де Люкс',
+		name2: 'Шефбургер Джуниор',
+		name3: 'Твистер Острый',
+		name4: 'Твистер Оригинальный',
+		name5: 'БоксМастер острый',
+		name6: 'БоксМастер оригинальный',
+		name7: '2 Боксмастера',
+
+		price1: '164р',
+		price2: '99р',
+		price3: '184р',
+		price4: '184р',
+		price5: '219р',
+		price6: '219р',
+		price7: '379р',
+	}
+});
